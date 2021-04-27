38150d3522bd21b07a86fb6bfeeffe33b997b33d
Arjun <44180430+ArjunSahlot@users.noreply.github.com>
2021-04-26 23:40:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="module",
+    name="pyml",
     version="0.0.1",
     author="Arjun Sahlot",
     author_email="iarjun.sahlot@gmail.com",
-    description="Module with various useful utilities.",
+    description="Machine learning library made from scratch",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ArjunSahlot/",
-    py_modules=["module"],
+    py_modules=["pyml"],
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy"
