64bcb6460fff6eb4b3ad8ac14929922c50bff1e3
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-06-25 23:50:45 -0700

    optims

---
+++ tensornn/optimizers.py
@@ -0,0 +1,28 @@
+"""
+This file contains optimizers that help tune your neural network.
+Optimizers enable us to improve our neural network efficiently.
+"""
+
+#
+#  TensorNN
+#  Python machine learning library/framework made from scratch.
+#  Copyright Arjun Sahlot 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+
+class Optimizer:
+    pass
