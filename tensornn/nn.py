977c0d86868955844f80f1db30f3a494f2e74a1c
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-07-21 16:35:33 -0700

    base network

--- tensornn/nn.py
+++ tensornn/nn.py
@@ -22,10 +22,13 @@ a collections of layers.
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-from typing import Sequence
+from typing import List, Optional, Sequence
 import numpy as np
 
 from .layers import Layer
+from .tensor import Tensor
+from .optimizers import Optimizer, Adam
+from .loss import Loss, CategoricalCrossEntropy
 
 
 __all__ = [
@@ -38,7 +41,43 @@ class NeuralNetwork:
     Create your neural network with this class.
     """
 
-    def __init__(self, layers: Sequence[Layer]) -> None:
-        self.layers = layers
+    def __init__(
+        self,
+        layers: Optional[Sequence[Layer]] = [],
+        loss: Optional[Loss] = CategoricalCrossEntropy(),
+        optimizer: Optional[Optimizer] = Adam()
+    ) -> None:
+        """
+        Initialize the network
 
-    # def
+        :param layers: list of layers that make up network
+        :param loss: type of loss this network uses to calculate loss
+        """
+
+        self.layers: List[Layer] = list(layers)
+        self.loss = loss
+        self.optimizer = optimizer
+
+    def add(self, layer: Layer) -> None:
+        """
+        Add another layer to the network. This is the same as initializing
+        the network with this layer.
+
+        :param layer: the layer to be added
+        :returns: nothing
+        """
+
+        self.layers.append(layer)
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        """
+        Get the output of the neural network.
+
+        :param inputs: inputs to the network, list of batches which contain a list of inputs
+        :returns: output of network after being passed through all layers
+        """
+
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+
+        return inputs
