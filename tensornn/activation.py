964de267401563fbd336e9cf1839242bd692daf0
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-09-09 23:21:08 -0700

    clean up activations

--- tensornn/activation.py
+++ tensornn/activation.py
@@ -59,9 +59,10 @@ class Activation(ABC):
 
 class ReLU(Activation):
     """
-    The rectified linear unit activation function is one of the simplest activation function.
+    The rectified linear unit activation function is one of the simplest activation function. It is
+    a piecewise function.
+    Formula: ``if x>=0, x; if x<0, 0``
 
-    If ReLU is given a negative value(<0) it will convert it into a 0, otherwise it will keep it the same.
     Ex: ``[12.319, -91.3, 0.132] -> [12.319, 0, 0.132]``
     """
 
@@ -74,49 +75,53 @@ class ReLU(Activation):
 
 class LeakyReLU(Activation):
     """
-    Leaky ReLU is extremely similar to ReLU but instead of just 0 for negatives, it is the leak times the value.
-    Ex, leak=0.1. ``[12.319, -91.3, 0.132] -> [12.319, -9.13, 0.132]``
+    Leaky ReLU is extremely similar to ReLU. ReLU is LeakyReLU if A was 0.
+    Formula: ``if x>=0, x; if x<0, Ax`` | constants: A(leak)
+
+    Ex, A=0.1 ``[12.319, -91.3, 0.132] -> [12.319, -9.13, 0.132]``
     """
 
-    def __init__(self, leak: float = 0.1) -> None:
+    def __init__(self, a: float = 0.1) -> None:
         """
         Initialize LeakyReLU.
 
         :param alpha: multiplier used in formula, checkout help(tnn.activation.LeakyReLU), defaults to 1
         :returns: nothing
         """
-        # TODO: enforce leak should be positive
+        # TODO: enforce a should be positive
 
-        self.leak = 0
+        self.a = 0
 
     def forward(self, inputs: Tensor) -> Tensor:
-        return np.where(inputs > 0, inputs, inputs * self.leak)
+        return np.where(inputs > 0, inputs, inputs * self.a)
 
     def backward(self, inputs: Tensor) -> Tensor:
-        return np.where(inputs > 0, 1, self.leak)
+        return np.where(inputs > 0, 1, self.a)
 
 
 class ELU(Activation):
     """
-    Exponential linear unit is itself for non negative values but otherwise it's ``alpha*((e^x)-1)``,
-    alpha can be any value.
+    Exponential linear unit is also a step function.
+    Formula: ``A*((e^x)-1)`` | constants: A, e(Euler's number, 2.718...)
     """
 
-    def __init__(self, alpha: float = 1) -> None:
+    # TODO: add ex
+
+    def __init__(self, a: float = 1) -> None:
         """
         Initialize ELU.
 
-        :param alpha: multiplier used in formula, checkout help(tnn.activation.ELU), defaults to 1
+        :param a: multiplier used in formula, checkout help(tnn.activation.ELU), defaults to 1
         :returns: nothing
         """
 
-        self.alpha = alpha
+        self.a = a
 
     def forward(self, inputs: Tensor) -> Tensor:
-        return np.where(inputs > 0, inputs, self.alpha * (np.exp(inputs) - 1))
+        return np.where(inputs > 0, inputs, self.a * (np.exp(inputs) - 1))
 
     def backward(self, inputs: Tensor) -> Tensor:
-        return np.where(inputs > 0, 1, self.alpha * np.exp(inputs))
+        return np.where(inputs > 0, 1, self.a * np.exp(inputs))
 
 
 class Softmax(Activation):
@@ -155,13 +160,17 @@ class Softmax(Activation):
         return exp / np.sum(exp, axis=int(inputs.ndim == 2))
 
     def backward(self, inputs: Tensor) -> Tensor:
+        # TODO: implement
         return
 
 
 class Sigmoid(Activation):
     """
-    The sigmoid function is the following: 1 / (1+e^(-x)). x being our inputs and e being euler's number.
+    The sigmoid function's output is always between -1 and 1
+    Formula: ``1 / (1+e^(-x))`` | constants: e(Euler's number, 2.718...)
     """
+
+    # TODO: add ex
 
     def forward(self, inputs: Tensor) -> Tensor:
         return 1 / (1 + np.exp(-inputs))
@@ -173,8 +182,11 @@ class Sigmoid(Activation):
 
 class Swish(Activation):
     """
-    The swish activation function is the output of the sigmoid function multiplied by x(inputs).
+    The swish activation function is the output of the sigmoid function multiplied by x.
+    Formula: ``x / (1+e^(-x))`` | constants: e(Euler's number, 2.718...)
     """
+
+    # TODO: add ex
 
     def forward(self, inputs: Tensor) -> Tensor:
         return inputs / (1 + np.exp(-inputs))
